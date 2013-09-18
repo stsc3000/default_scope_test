d67c583da1a1839ba9b270d44cee8ccf4046a8f3
sschildknecht <steffen.schildknecht@zweitag.de>
2013-09-18 11:40:39 +0200

    Add failing test

--- test/models/location_based_data_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class LocationBasedDataTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/location_based_meta_data_test.rb
+++ test/models/location_based_meta_data_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class LocationBasedMetaDataTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "A: joining with materials" do
+    location_based_meta_data_material_association
+  end
+
+  test "B: joining materials with squeel" do
+    require 'squeel'
+    location_based_meta_data_material_association
+  end
+
+  def location_based_meta_data_material_association
+    material = Material.create(name: "Dough")
+    location = Location.create(name: "Pizza Factory")
+    material.locations << location
+    location_based_data = material.location_based_datas.first
+    location_based_meta_data = LocationBasedMetaData.create(name: "Needs to be mixed")
+    location_based_data.location_based_meta_datas << location_based_meta_data
+
+    assert(
+      location_based_meta_data.material == material
+    )
+
+    assert(
+      LocationBasedMetaData.joins(:material).where("materials.name" => "Dough").to_a[0] ==
+      location_based_meta_data
+    )
+
+  end
+
 end
