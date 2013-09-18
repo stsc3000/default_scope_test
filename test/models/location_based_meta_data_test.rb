84d5d0d3b6a912dc94843764018659deb518b7d9
sschildknecht <steffen.schildknecht@zweitag.de>
2013-09-18 15:01:50 +0200

    Add squeel to test

--- test/models/location_based_meta_data_test.rb
+++ test/models/location_based_meta_data_test.rb
@@ -1,33 +1,37 @@
 require 'test_helper'
+#Works without squeel, doesn't when squeel is included
+require 'squeel'
 
 class LocationBasedMetaDataTest < ActiveSupport::TestCase
 
-  test "A: joining with materials" do
-    location_based_meta_data_material_association
+  def check_out_location_based_meta_data_material_association
+    assert(
+      @location_based_meta_data.reload.material == @material
+    )
   end
 
-  test "B: joining materials with squeel" do
-    require 'squeel'
-    location_based_meta_data_material_association
+  def check_out_location_based_meta_data_materials_join
+    assert(
+      LocationBasedMetaData.joins(:material).where( materials: { name: "Dough" } ).to_a[0] ==
+      @location_based_meta_data.reload
+    )
   end
 
-  def location_based_meta_data_material_association
-    material = Material.create(name: "Dough")
-    location = Location.create(name: "Pizza Factory")
-    material.locations << location
-    location_based_data = material.location_based_datas.first
-    location_based_meta_data = LocationBasedMetaData.create(name: "Needs to be mixed")
-    location_based_data.location_based_meta_datas << location_based_meta_data
+  def setup
+    @material = Material.create(name: "Dough")
+    @location = Location.create(name: "Pizza Factory")
+    @material.locations << @location
+    @location_based_data = @material.location_based_datas.first
+    @location_based_meta_data = LocationBasedMetaData.create(name: "Needs to be mixed")
+    @location_based_data.location_based_meta_datas << @location_based_meta_data
+  end
 
-    assert(
-      location_based_meta_data.material == material
-    )
+  test "material association" do
+    check_out_location_based_meta_data_material_association
+  end
 
-    assert(
-      LocationBasedMetaData.joins(:material).where("materials.name" => "Dough").to_a[0] ==
-      location_based_meta_data
-    )
-
+  test "joins with materials" do
+    check_out_location_based_meta_data_materials_join
   end
 
 end
